5cc5c13526158d7c5c17f7a38258a5227c35efed
Nikos Nikoleris <nikos.nikoleris@arm.com>
2024-04-03 17:32:09 +0100

    [herd] Add a flag and catalogue with rules on AArch64 Load/Store Exclusives
    Signed-off-by: Nikos Nikoleris <nikos.nikoleris@arm.com>

---
+++ catalogue/aarch64-lxsx/shelf.py
@@ -0,0 +1,19 @@
+record = "AArch64-lxsx"
+
+cats = [
+    "cats/aarch64.cat",
+    ]
+
+cfgs = [
+    "cfgs/new-web.cfg",
+]
+
+illustrative_tests = [
+    "tests/coRW-lxsx+R.litmus",
+    "tests/coRW-lxsx+dc.cvau.litmus",
+    "tests/coRW-lxsx+eret.litmus",
+    "tests/coRW-lxsx+fault.litmus",
+    "tests/coRW-lxsx+ic.ivau.litmus",
+    "tests/coRW-lxsx+isb.litmus",
+    "tests/coRW-lxsx+tlbi.alle1.litmus"
+]
