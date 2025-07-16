4c6f7666b87b69e492cd846e7eaf7aa83ed5cdc9
Nikos Nikoleris <nikos.nikoleris@arm.com>
2025-07-16 21:07:08 +0100

    [Makefile] Fix aarch64-mixed regressions run

--- catalogue/aarch64-mixed/shelf.py
+++ catalogue/aarch64-mixed/shelf.py
@@ -11,12 +11,16 @@ cfgs = [
 illustrative_tests = [
     "tests/MP-Koeln.litmus",
     "tests/LB+dmb.sy+data-wsi-wsi+MIXED+H.litmus",
+    "tests/LB+dmb.sy+data-wsi-wsi+MIXED.litmus",
     "tests/MP+HAmo+BAcqAmo.litmus",
+    "tests/MP+QAmo+AcqAmo.litmus",
     "tests/CO-MIXED-20cc+H.litmus",
+    "tests/CO-MIXED-20cc.litmus",
     "tests/CoRW2+posh0h0+h0.litmus",
     "tests/CoRW2+posb0b0+b0.litmus",
     "tests/CoRW2+posb1b0+h0.litmus",
     "tests/2+2W+posb0b0+posb1b1.litmus",
     "tests/MP+dmb.syb0b1+datab1b1-rfib1h0.litmus",
+    "tests/MP+popl.w0-posl.w0q0+poa.w4p.litmus",
     "tests/SmallEndian.litmus",
 ]
