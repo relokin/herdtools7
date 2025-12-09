a773302fc77ac830442b9dd19d0b7b1481d1c931
Nikos Nikoleris <nikos.nikoleris@arm.com>
2025-12-09 13:44:39 +0000

    [catalogue] Add a shelf.py and a cfg for AArch64-GCS
    Signed-off-by: Nikos Nikoleris <nikos.nikoleris@arm.com>

---
+++ catalogue/aarch64-GCS/shelf.py
@@ -0,0 +1,52 @@
+record = "AArch64 GCS"
+
+cats = [
+    "cats/aarch64.cat",
+]
+
+cfgs = [
+    "cfgs/herd.cfg",
+]
+
+illustrative_tests = [
+    "tests/MP+ptev0-dsb.st-tlbi.vaae1is-dsb.ish-p+preload-dmb.ish-gcsb.dsync-ret.litmus",
+    "tests/RFZRGP-000a.litmus",
+    "tests/RFZRGP-000b.litmus",
+    "tests/RFZRGP-001a.litmus",
+    "tests/RFZRGP-001b.litmus",
+    "tests/RFZRGP-002a.litmus",
+    "tests/RFZRGP-002b.litmus",
+    "tests/RFZRGP-003.litmus",
+    "tests/RFZRGP-004.litmus",
+    "tests/RFZRGP-005.litmus",
+    "tests/RFZRGP-006.litmus",
+    "tests/RFZRGP-007.litmus",
+    "tests/RZRZQM-000a.litmus",
+    "tests/RZRZQM-000b.litmus",
+    "tests/RZRZQM-000c.litmus",
+    "tests/RZRZQM-000d.litmus",
+    "tests/RZRZQM-000e.litmus",
+    "tests/RZRZQM-000f.litmus",
+    "tests/RZRZQM-001a.litmus",
+    "tests/RZRZQM-001b.litmus",
+    "tests/RZRZQM-001c.litmus",
+    "tests/RZRZQM-002a.litmus",
+    "tests/RZRZQM-002b.litmus",
+    "tests/RZRZQM-002c.litmus",
+    "tests/RZRZQM-002d.litmus",
+    "tests/RZRZQM-002e.litmus",
+    "tests/RZRZQM-002f.litmus",
+    "tests/RZRZQM-003a.litmus",
+    "tests/RZRZQM-003b.litmus",
+    "tests/RZRZQM-003c.litmus",
+    "tests/RZRZQM-004a.litmus",
+    "tests/RZRZQM-004b.litmus",
+    "tests/RZRZQM-004c.litmus",
+    "tests/RZRZQM-005a.litmus",
+    "tests/RZRZQM-005b.litmus",
+    "tests/RZRZQM-005c.litmus",
+    "tests/RZRZQM-005d.litmus",
+    "tests/RZRZQM-005e.litmus",
+    "tests/RZRZQM-005f.litmus",
+    "tests/WW+R+pospptev1.litmus",
+]
